06e763c24e97f0fcdee3ca7a72252246999233ba
manoti3 <hillarymanoti0@gmail.com>
2023-12-15 12:22:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser = 'Hillary Manoti!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, HILLARY MANOTI!';
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
